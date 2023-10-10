cac9cc9280504b610e08f4c1f58ea9e8be2297b1
hanoch <hanokteja75@gmail.com>
2023-10-10 13:28:18 +0000

    commiting from local master to see the error

--- Hello.java
+++ Hello.java
@@ -5,4 +5,4 @@ Public static void main(String args[])
 System.out.println(“hello from master branch”);
 }
 }
-this am commiting first from local master 
+first changes made in remote and then this is local master commiting 
